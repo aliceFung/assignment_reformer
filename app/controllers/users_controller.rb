74ba46b711d6f3bb6ea3c7ba88a4624970bc73d5
facingsouth <xin.d.liu@gmail.com>
2015-07-25 11:23:47 -0700

    finish form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
 
   def whitelisted_user_params
     params.require(:user).permit(:username, :email, :password)
