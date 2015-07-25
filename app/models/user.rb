dd6025ef4e34efd155a4f1e0698a33001562ccfe
Alice Fung <alice.o.fung@gmail.com>
2015-07-25 12:58:04 -0400

    tested out different validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  validates :username, :email, :password
+  validates :username,
+            :length => {maximum: 5},
+            :allow_blank => true,
+            :strict => true
+
+  validates :email,
+            :presence => true,
+            :uniqueness => true,
+            :length => {maximum: 15}
+
+  validates :password,
+            :presence => true
 end
