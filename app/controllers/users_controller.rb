598ad5ec926c58c253f87c72460fa00168926863
facingsouth <xin.d.liu@gmail.com>
2015-07-25 11:08:06 -0700

    finish form tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(whitelisted_user_params)
+    @user = User.new(check_params)
     if @user.save
       redirect_to new_user_path
     else
@@ -16,5 +16,15 @@ class UsersController < ApplicationController
 
   def whitelisted_user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def check_params
+    if params[:user]
+      whitelisted_user_params
+    else
+      {:username => params[:username],
+       :email    => params[:email], 
+       :password => params[:password]}
+   end
   end
 end
